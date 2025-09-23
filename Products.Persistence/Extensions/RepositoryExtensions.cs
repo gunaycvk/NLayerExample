09cb0c1351fd3064e55de9b0617a4e64f54e0a80
gunaycvk <gunay.cvk782@icloud.com>
2025-09-23 21:48:59 +0300

    Add project files.

---
+++ Products.Persistence/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Persistence.Extensions
+{
+    internal class RepositoryExtensions
+    {
+    }
+}
---
+++ WebApplication1/ExceptionHandler/CriticalExceptionHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.ExceptionHandler
+{
+    public class CriticalExceptionHandler
+    {
+    }
+}
